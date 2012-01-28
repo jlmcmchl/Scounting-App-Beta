e85a6dd91ebddbbb48b5b021dccfca685b68dc6b
Team RUSH 1 <TeamRUSH1@Team-RUSH-1s-MacBook-Pro.local>
2012-01-27 21:04:25 -0500

    Initial Commit

---
+++ Scounting App BetaTests/Scounting_App_BetaTests.h
@@ -0,0 +1,13 @@
+//
+//  Scounting_App_BetaTests.h
+//  Scounting App BetaTests
+//
+//  Created by Kyle Hughes on 1/27/12.
+//  Copyright (c) 2012 Team RUSH . All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Scounting_App_BetaTests : SenTestCase
+
+@end
---
+++ Scounting App BetaTests/Scounting_App_BetaTests.m
@@ -0,0 +1,32 @@
+//
+//  Scounting_App_BetaTests.m
+//  Scounting App BetaTests
+//
+//  Created by Kyle Hughes on 1/27/12.
+//  Copyright (c) 2012 Team RUSH . All rights reserved.
+//
+
+#import "Scounting_App_BetaTests.h"
+
+@implementation Scounting_App_BetaTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Scounting App BetaTests");
+}
+
+@end
