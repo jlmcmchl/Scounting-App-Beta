63eeb0e737c61e5e80c1c5ab2bb5a2147e2c35a8
Jordan McMichael <jlmcmchl@gmail.com>
2015-01-06 19:52:37 -0500

    Initial Commit
    Initial Commit

---
+++ Scounting App Beta/FourthViewController.h
@@ -0,0 +1,13 @@
+//
+//  FourthViewController.h
+//  Scounting App Beta
+//
+//  Created by Kyle Hughes on 1/28/12.
+//  Copyright (c) 2012 Team RUSH . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FourthViewController : UIViewController
+
+@end
---
+++ Scounting App Beta/ThirdViewController.h
@@ -0,0 +1,13 @@
+//
+//  ThirdViewController.h
+//  Scounting App Beta
+//
+//  Created by Kyle Hughes on 1/28/12.
+//  Copyright (c) 2012 Team RUSH . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ThirdViewController : UIViewController
+
+@end
